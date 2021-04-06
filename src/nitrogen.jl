741112bd1996c12e61fd53c06c14e427bcf966f0
Kyungdahm Yun <tomyun@gmail.com>
2021-04-06 15:52:18 -0700

    Set default parameters for SPAD-N conversion

--- src/nitrogen.jl
+++ src/nitrogen.jl
@@ -1,8 +1,8 @@
 @system Nitrogen begin
-    SPAD: SPAD_greenness ~ preserve(parameter)
-    SNa: SPAD_N_coeff_a ~ preserve(u"g/m^2", parameter)
-    SNb: SPAD_N_coeff_b ~ preserve(u"g/m^2", parameter)
-    SNc: SPAD_N_coeff_c ~ preserve(u"g/m^2", parameter)
+    SPAD: SPAD_greenness => 60 ~ preserve(parameter)
+    SNa: SPAD_N_coeff_a => 0.0004 ~ preserve(u"g/m^2", parameter)
+    SNb: SPAD_N_coeff_b => 0.012 ~ preserve(u"g/m^2", parameter)
+    SNc: SPAD_N_coeff_c => 0 ~ preserve(u"g/m^2", parameter)
     N(SPAD, a=SNa, b=SNb, c=SNc): leaf_nitrogen_content => begin
         a*SPAD^2 + b*SPAD + c
     end ~ preserve(u"g/m^2", parameter)
